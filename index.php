fea6c9c8c59ad88eda92a0a47bc9c078ea98f1f9
Florian Weidinger <CrasyHorse@gmx.net>
2021-01-17 13:19:17 +0100

    Added route configuration

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once('vendor/autoload.php');
 
 $f3 = Base::instance();
 $f3->set('AUTOLOAD', 'app/');
-
-$f3->map('/', 'Shorty\Http\Controllers\AbbreviationController');
+$f3->config('config/config.ini');
 
 $f3->run();
