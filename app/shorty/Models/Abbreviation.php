da78c4fcad89b98b6206cf46bb95833f71ed3fbd
Florian Weidinger <CrasyHorse@gmx.net>
2021-01-24 20:04:28 +0100

    Implemented fill method

--- app/shorty/Models/Abbreviation.php
+++ app/shorty/Models/Abbreviation.php
@@ -10,9 +10,22 @@ class Abbreviation extends BaseModel
 {
     private $table_name = 'abbreviations';
 
-    public function __construct()
+    public function __construct(array $request = null)
     {
         parent::__construct($this->table_name);
+
+        $this->fillable = ['id', 'short', 'long'];
+        
+        if (!empty($request)) {
+            $this->fill($request);
+        }
+    }
+
+    protected function fillAttributes(array $values): void
+    {
+        foreach ($this->fillable as $attribute) {
+            $this->{$attribute} = $values[$attribute];
+        }
     }
 
 }
