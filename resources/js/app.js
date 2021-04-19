deac004fa940c82ef3a28f370290702c6e6fa6bf
dominikthomas <dominikthomas@bundeswehr.org>
2021-04-19 14:32:29 +0200

    Added_component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,25 @@
+Vue.component('sz-navbar', {
+    template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" 
+           href="#"
+        >
+           <img src="resources/img/Wappen_SZ25_web_klein.PNG">
+           <span>Sysz25 - Shorty</span>
+        </a>
+        <button 
+            class="navbar-toggler" 
+            type="button" 
+            aria-controls="navbarSupportedContent" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+`
+});
+
 const vm = new Vue({
     el: '#app'
 });
