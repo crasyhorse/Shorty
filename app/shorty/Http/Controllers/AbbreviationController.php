1cf0bc198aee5da22b63a34e2c0edd9fded40c6d
dominikthomas <dominikthomas@bundeswehr.org>
2021-04-19 10:49:20 +0200

    Controller Changes

--- app/shorty/Http/Controllers/AbbreviationController.php
+++ app/shorty/Http/Controllers/AbbreviationController.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Shorty\Http\Controllers;
 
 use Base;
+use Exception;
+use PDOException;
 use Shorty\Models\Abbreviation;
 use Template;
+use Throwable;
 
 class AbbreviationController
 {
@@ -25,9 +28,12 @@ class AbbreviationController
     public function post(): void
     {
         $request = $this->f3->get('POST');
-
-        $abbreviation = new Abbreviation($request);
-        $abbreviation->save();
+        try {
+            $abbreviation = new Abbreviation($request);
+            $abbreviation->save();
+        } catch (Throwable $t) {
+            throw new PDOException();
+        }
     }
 
     public function patch(): void
@@ -37,6 +43,11 @@ class AbbreviationController
         $abbreviation = (new Abbreviation())->load(
             ['id = :id', ':id' => $id]
         );
+
+        if (!$abbreviation) {
+            throw new Exception();
+        }
+
         $abbreviation->short = $request['short'];
         $abbreviation->long = $request['long'];
         $abbreviation->save();
