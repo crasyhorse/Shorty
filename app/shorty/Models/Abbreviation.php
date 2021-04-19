49e0cef570a4ab78edee877ce7592d9cf42bb98e
dominikthomas <dominikthomas@bundeswehr.org>
2021-04-19 08:55:23 +0200

    Added Fillable

--- app/shorty/Models/Abbreviation.php
+++ app/shorty/Models/Abbreviation.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace Shorty\Models;
 
-use Shorty\Models\BaseModel;
-
 class Abbreviation extends BaseModel
 {
     private $table_name = 'abbreviations';
 
-    public function __construct()
+    public function __construct(array $request = null)
     {
         parent::__construct($this->table_name);
+
+        $this->fillable = ['id', 'short', 'long'];
+
+        if (!empty($request)) {
+            $this->fill($request);
+        }
     }
 
+    protected function fillAttributes(array $values): void
+    {
+        foreach ($this->fillable as $attribute) {
+            $this->{$attribute} = $values[$attribute];
+        }
+    }
 }
